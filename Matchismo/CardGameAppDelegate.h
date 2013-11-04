557c5432b875aa9511e129fd9087ac041cbc5b46
Michael Mendoza <mmendoza27@me.com>
2013-11-04 14:35:22 -0600

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Michael Mendoza on 11/4/13.
+//  Copyright (c) 2013 Dangerous Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
