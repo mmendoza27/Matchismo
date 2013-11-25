9e355545c0f834d575c341c6f8fab571ea6feb27
Michael Mendoza <mmendoza27@me.com>
2013-11-24 23:55:15 -0600

    Homework has been completed. Fixed issue where we would only create 92 cards since we were not going up to MaxRank when allocating our PlayingCardDeck. Also finished UI and will flip through whole deck.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,15 +7,26 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-
+@property (strong  , nonatomic) Deck *cards;
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
 
+- (Deck *)cards {
+   if (!_cards) {
+      _cards = [self createDeck];
+   }
+   return _cards;
+}
+
+- (Deck *)createDeck {
+   return [[PlayingCardDeck alloc] init];
+}
 
 - (void)setFlipCount:(int)flipCount {
    _flipCount = flipCount;
@@ -28,12 +39,17 @@
       [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
                         forState:UIControlStateNormal];
       [sender setTitle:@"" forState:UIControlStateNormal];
+      self.flipCount++;
    } else {
-      [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
-                        forState:UIControlStateNormal];
-      [sender setTitle:@"A♣️" forState:UIControlStateNormal];
+      Card *randomCard = [self.cards drawRandomCard];
+      
+      if (randomCard) {
+         [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
+                           forState:UIControlStateNormal];
+         [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+         self.flipCount++;
+      }
    }
-   self.flipCount++;
 }
 
 @end
