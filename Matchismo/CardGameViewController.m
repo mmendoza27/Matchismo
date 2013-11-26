9eb5ad41cc77b02d9646dfaee9056ccab2c6d2d5
mmendoza27 <mmendoza27@me.com>
2013-11-25 22:02:22 -0600

    Completed and added all modifications from Lecture 3. Assignment 2 is ready to begin.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -12,6 +12,7 @@
 
 @interface CardGameViewController ()
 @property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @end
 
@@ -36,7 +37,27 @@
 }
 
 - (void)updateUI {
-   
+   for (UIButton *cardButton in self.cardButtons) {
+      NSUInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+      Card *card = [self.game cardAtIndex:cardButtonIndex];
+      
+      [cardButton setTitle:[self titleForCard:card]
+                  forState:UIControlStateNormal];
+      [cardButton setBackgroundImage:[self backgroundImageForCard:card]
+                            forState:UIControlStateNormal];
+      
+      cardButton.enabled = !card.isMatched;
+      self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld",
+                              (long)self.game.score];
+   }
+}
+
+- (NSString *)titleForCard:(Card *)card {
+   return card.isChosen ? card.contents : @"";
+}
+
+- (UIImage *)backgroundImageForCard:(Card *)card {
+   return [UIImage imageNamed:card.isChosen ? @"cardFront" : @"cardBack"];
 }
 
 @end
