293fb68017086bc9348cea6b7e225daebc51dffd
Michael Mendoza <mmendoza27@me.com>
2013-11-25 21:18:39 -0600

    Made minor adjustments to the storyboard.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -11,46 +11,32 @@
 #import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
-@property (strong  , nonatomic) Deck *cards;
-@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
-@property (nonatomic) int flipCount;
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)cards {
-   if (!_cards) {
-      _cards = [self createDeck];
+- (CardMatchingGame *)game {
+   if (!_game) {
+      _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                usingDeck:[self createDeck]];
    }
-   return _cards;
+   return _game;
 }
 
 - (Deck *)createDeck {
    return [[PlayingCardDeck alloc] init];
 }
 
-- (void)setFlipCount:(int)flipCount {
-   _flipCount = flipCount;
-   self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-   NSLog(@"flipCount changed to %d", flipCount);
+- (IBAction)touchCardButton:(UIButton *)sender {
+   NSUInteger chosenButtonIndex = [self.cardButtons indexOfObject:sender];
+   [self.game chooseCardAtIndex:chosenButtonIndex];
+   [self updateUI];
 }
 
-- (IBAction)touchCardButton:(UIButton *)sender {
-   if ([sender.currentTitle length]) {
-      [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
-                        forState:UIControlStateNormal];
-      [sender setTitle:@"" forState:UIControlStateNormal];
-      self.flipCount++;
-   } else {
-      Card *randomCard = [self.cards drawRandomCard];
-      
-      if (randomCard) {
-         [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
-                           forState:UIControlStateNormal];
-         [sender setTitle:randomCard.contents forState:UIControlStateNormal];
-         self.flipCount++;
-      }
-   }
+- (void)updateUI {
+   
 }
 
 @end
